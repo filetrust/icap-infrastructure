f277f4089eef37c1762b536d842e875431cc5bf1
Sylvester Loreto <sylvester.loreto@codurance.com>
2021-01-21 11:26:18 +0000

    Images can now be pulled and pushed to it's final container registry

--- push_images.sh
+++ push_images.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 main_dir="$PWD"
-rm -R -f "$main_dir/_images"
-mkdir "$main_dir/_images"
 images_dir="$main_dir/_images"
-
-final_registry="gwicapcontainerregistry.azurecr.io/"
+glasswallRegistry="gwicapcontainerregistry.azurecr.io/"
+finalRegistry="$1"
+echo "Final registry is:"
+echo $finalRegistry
 
 checkPrereqs() {
   declare -a tools=("az yq find docker")
@@ -30,48 +30,42 @@ checkPrereqs() {
 #  printf "Login succeeded\n"
 #}
 
-pullImages() {
+importImages() {
+
   for img in $(yq read -p p values.yaml 'imagestore.*'); do
     registry=$(yq read values.yaml "$img.registry")
     repository=$(yq read values.yaml "$img.repository")
     tag=$(yq read values.yaml "$img.tag")
-    printf "  %s\t->\t%s\n" "Registry" "$registry"
-    printf "  %s\t->\t%s\n" "Repository" "$repository"
-    printf "  %s\t->\t%s\n" "Tag" "$tag"
-    printf "  %s\t->\t%s\n" "$registry$repository:$tag" "$final_registry$repository:$tag"
-    docker pull -q "$registry$repository:$tag" >/dev/null
-    docker tag "$registry$repository:$tag" "$final_registry$repository:$tag"
-
     repository_name=${repository%%/*}
     image_name=${repository##*/}
+
     echo "Repository name:" "$repository_name"
     echo "Image name is:" "$image_name"
+    printf "  %s\t->\t%s\n" "$registry$repository:$tag" "gwicapcontainerregistry.azurecr.io/$repository:$tag"
 
-    current_helm_chart_dir=$images_dir/$final_registry$repository_name
-    mkdir -p $current_helm_chart_dir
-    echo "Current helm chart destination directory..." "$current_helm_chart_dir"
+    images_directory="_images"
+    gw_image_full_name="$glasswallRegistry$repository_name/$image_name:$tag"
+    client_image_full_name="$finalRegistry/$repository_name/$image_name:$tag"
+    image_relative_path="$images_dir/$gw_image_full_name.tgz"
+    printf "\n  %s\t->\t%s\n" "Final file name:" "$gw_image_full_name"
+    printf "\n  %s\t->\t%s\n\n" "Image imported:" "$image_relative_path"
 
-    final_file_name="$current_helm_chart_dir/$image_name:$tag.tgz"
-    echo "Final file name" "$final_file_name"
+    echo ""
+    echo "$PWD"
+    echo ""
+    docker import "$image_relative_path" $gw_image_full_name
+    echo "Image imported:" $gw_image_full_name
 
-    docker save $registry$repository:$tag > $final_file_name
-    echo "Image saved"
+    docker tag "$gw_image_full_name" "$client_image_full_name"
+    echo "Image tagged:" $client_image_full_name
+
+    docker push $client_image_full_name
+    echo "Image pushed:" $client_image_full_name
     echo ""
     echo ""
     echo ""
     echo ""
   done
-}
-
-createTarFile() {
-  tar_file="_images.tgz"
-  if [ -f "$tar_file" ]; then
-    echo "Removing '_images.tgz'..."
-    rm _images.tgz
-    echo "'_images.tgz' removed."
-  fi
-
-  rm -rf "$images_dir/$final_registry/*.tgz" && tar cvfz _images.tgz _images
 }
 
 checkPrereqs
@@ -81,17 +75,17 @@ set -o pipefail
 
 #registriesLogin
 
+#tar -zxvf _images.tgz
+
 find . -maxdepth 1 -mindepth 1 -type d | while read -r d; do
   cd $d
 
   if [[ -f values.yaml ]]; then
     printf "\nNow processing %s\n" "$d"
-    pullImages
+    importImages
     printf "\n"
   else
     printf "Skipping %s; no values file\n" "$d"
   fi
   cd ..
 done
-
-createTarFile
